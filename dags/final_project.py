9f5dadf5bd06f6daf822b5a51bfe2ff01958be45
Victor Manchik <vmanchik@fullcoll.edu>
2024-12-28 00:19:02 -0800

    Update final_project.py

--- dags/final_project.py
+++ dags/final_project.py
@@ -22,7 +22,7 @@ default_args = {
 @dag(
     default_args=default_args,
     description='Load and transform data in Redshift with Airflow',
-    schedule_interval='@daily'
+    schedule_interval='@hourly'
 )
 def final_project():
     """
