f0cf0d1cad42914a4f1a451855f8a82d1dfefabb
Victor Manchik <vmanchik@fullcoll.edu>
2024-12-27 22:50:28 -0800

    Update stage_redshift.py

--- operators/stage_redshift.py
+++ operators/stage_redshift.py
@@ -4,6 +4,18 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
+    """
+    Custom Airflow Operator to stage data from S3 to Redshift.
+
+    Attributes:
+        ui_color (str): Color code for the operator in the Airflow UI.
+        template_fields (tuple): Fields that will be templated by Jinja.
+        copy_sql (str): SQL command template for copying data from S3 to Redshift.
+
+    Methods:
+        __init__: Initializes the operator with the specified parameters.
+        execute: Executes the operator's logic to stage data from S3 to Redshift.
+    """
     ui_color = '#358140'
     template_fields = ("s3_path",)
     copy_sql = """
@@ -24,7 +36,19 @@ class StageToRedshiftOperator(BaseOperator):
                  region='us-west-2',
                  json_path='auto',
                  *args, **kwargs):
+        """
+        Initializes the StageToRedshiftOperator.
 
+        Args:
+            redshift_conn_id (str): Airflow connection ID for Redshift.
+            aws_credentials_id (str): Airflow connection ID for AWS credentials.
+            table (str): The target Redshift table to copy data into.
+            s3_path (str): The S3 path where the data is stored.
+            region (str): The AWS region where the S3 bucket is located.
+            json_path (str): The JSON path file to be used for data parsing.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.aws_credentials_id = aws_credentials_id
@@ -34,6 +58,15 @@ class StageToRedshiftOperator(BaseOperator):
         self.json_path = json_path
 
     def execute(self, context):
+        """
+        Executes the data staging from S3 to Redshift.
+
+        This method fetches AWS credentials, clears the target Redshift table, 
+        renders the S3 path, formats the SQL copy command, and runs it on Redshift.
+
+        Args:
+            context (dict): Airflow context dictionary.
+        """
         aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
